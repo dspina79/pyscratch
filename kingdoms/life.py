7af80a9a4d220ca5a8b2e90ea73d8e1c2c3ac692
Dave Spina <dspina79@gmail.com>
2022-04-05 21:06:07 -0400

    Stated kingdom elements

---
+++ kingdoms/life.py
@@ -0,0 +1,28 @@
+# Life of Pi
+
+class LivingThing:
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+    
+    def speak(self):
+        print(self.name, "says hello in its own way.")
+
+class Monera(LivingThing):
+    def __init__(self, name):
+        super().__init__(name)
+        self.cells = 1
+    
+    def divide(self):
+        kid_name = self.name + "'s kid"
+        print("Dividing and creating", kid_name)
+        return Monera(kid_name)
+
+
+imp = LivingThing("Ted")
+imp.speak()
+
+monte = Monera("Monte")
+monte.speak()
+child_1 = monte.divide()
+child_1.speak()
