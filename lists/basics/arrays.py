69361f74470bdf8894f8651d59aa19908c233b55
Dave Spina <dspina79@gmail.com>
2022-04-06 20:40:17 -0400

    Started lists

---
+++ lists/basics/arrays.py
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5,6,7]
+
+print(arr) # print the array
+print(arr[1:]) # print everything from the 1 index onward
+print(arr[::]) # print all
+print(arr[-2:]) # last two elements
+print(arr[1:4]) # prints the 2nd to 4th element (inclusive)
