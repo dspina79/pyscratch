9b787b72e306e07cb792c0cfba2c180e307f62ab
Dave Spina <dspina79@gmail.com>
2022-04-10 12:05:15 -0400

    Finished up queue

--- lists/vectors/queue.py
+++ lists/vectors/queue.py
@@ -7,15 +7,28 @@ class Queue:
         self.arr.append(item)
     
     def pop(self):
-        if self.arr.count() > 0:
+        if self.arr.__len__() > 0:
             return self.arr.pop(0)
         else:
             return None
 
     def count(self):
-        return self.arr.count()
+        return self.arr.__len__()
 
     def print(self):
-        print("There are", self.arr.count(), "items in the queue.")
+        print("There are", self.arr.__len__(), "items in the queue.")
         print(self.arr)
+
+q = Queue()
+q.add("Ted")
+q.add("Carmen")
+q.add("Steve")
+q.add("Linda")
+q.add("Bryce")
+
+q.print()
+while q.count() > 0:
+    item = q.pop()
+    print(item, "came off the queue")
+    q.print()
 
