427ae44139a3ec7f8b2d7034d0f3d4390a185f73
Dave Spina <dspina79@gmail.com>
2022-04-13 20:56:54 -0400

    Added rectangle

---
+++ mather/rectangle.py
@@ -0,0 +1,26 @@
+class Rectangle:
+    def __init__(self, width, height):
+        super().__init__()
+        self.height = height
+        self.width = width
+    
+    def isSquare(self):
+        return self.width == self.height
+    
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * self.width + self.height
+
+
+s = Rectangle(4, 4)
+r = Rectangle(3, 4)
+
+print("s.isSquare:", s.isSquare())
+print("s.area:", s.area())
+print("s.perimter:", s.perimeter())
+
+print("r.isSquare:", r.isSquare())
+print("r.area:", r.area())
+print("r.perimter:", r.perimeter())
