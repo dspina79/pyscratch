00addc0652a40e2c4bdca59f2c34ede83f24082a
Dave Spina <dspina79@gmail.com>
2022-04-14 22:10:15 -0400

    Minor adjustments

---
+++ life/lifeelement.py
@@ -0,0 +1,27 @@
+class Life:
+    def __init__(self):
+        super().__init__()
+        self.complexity = 1
+        self.lifespan = 0.01
+    
+    def foodRequirement(self):
+        return self.complexity * 1.63 
+
+    def eat(self, amt):
+        self.foodAmount += amt
+
+    def metabolize(self):
+        self.foodAmount -= self.foodRequirement()
+        self.age += 0.001
+
+    def is_living(self):
+        return (self.age < self.lifespan) and self.foodAmount > (self.foodRequirement * -2)
+
+    def reproduce(self):
+        return Life()
+
+    def pulse(self, foodAmt):
+        if (self.is_living()):
+            self.eat(foodAmt)
+            self.metabolize()
+
