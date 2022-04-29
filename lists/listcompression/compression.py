fe65c3c1017a380f410740d2e13780fabde47dc3
Dave Spina <dspina79@gmail.com>
2022-04-28 21:00:57 -0400

    More list compression

--- lists/listcompression/compression.py
+++ lists/listcompression/compression.py
@@ -3,3 +3,8 @@ new_list = [name for name in names if "a" in name ]
 print(new_list)
 
 # outputs ['Dean', 'Linda', 'Adam']
+
+nums = [1,2,3,5,8,13,21,34]
+evens = [num for num in nums if num % 2 == 0]
+print(evens)
+# prints [2, 8, 34]
