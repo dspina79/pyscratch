b0b13315278cbc7bf8c0af013203a8963db92836
Dave Spina <dspina79@gmail.com>
2022-05-01 20:50:33 -0400

    Started data manager

---
+++ serialization/example2/dbmanager.py
@@ -0,0 +1,23 @@
+import serialization.example2.accounts
+import pickle
+
+class DatabaseManager:
+    def __init__(self, dbpath):
+        super().__init__()
+        self.path = dbpath
+        self.manager = None
+
+    def refresh_data(self):
+        f = open(self.path, 'rb')
+        self.manager = pickle.load(f)
+        f.close()
+        return manager
+    
+    def write_data(self):
+        if self.manager != None:
+            f = open(self.path, 'wb')
+            pickle.dump(self.manager, f)
+            f.close()
+
+
+        
