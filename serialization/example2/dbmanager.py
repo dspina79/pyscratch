896b5412a0f5e70f1aa7840407609de50aed0371
Dave Spina <dspina79@gmail.com>
2022-05-02 21:20:26 -0400

    Instantiated writing data

--- serialization/example2/dbmanager.py
+++ serialization/example2/dbmanager.py
@@ -1,11 +1,11 @@
-import accounts
+import accounts as a
 import pickle
 
 class DatabaseManager:
     def __init__(self, dbpath):
         super().__init__()
         self.path = dbpath
-        self.manager = None
+        self.manager = a.AccountManager()
 
     def refresh_data(self):
         f = open(self.path, 'rb')
@@ -20,4 +20,10 @@ class DatabaseManager:
             f.close()
 
 
-        
+dmgr = DatabaseManager('serialization/example2/maindata.txt')
+acctmgr = dmgr.manager
+acct1 = a.Account('Dean', 'Williams', 'dwilliams@nowhere.net', 53)
+acct2 = a.Account('Sharon', 'Folsom', 'sforlsm@nowhere.net', 23)
+acctmgr.append(acct1)
+acctmgr.append(acct2)
+dmgr.write_data()        
