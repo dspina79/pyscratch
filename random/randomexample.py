1a17cf12d368ea03a3ec1231aa6d5db79599a92d
Dave Spina <dspina79@gmail.com>
2022-05-13 23:17:28 -0400

    Updated iteration and new sequence builder

---
+++ random/randomexample.py
@@ -0,0 +1,24 @@
+import random as r
+
+LOWER_BOUND = -1000
+UPPER_BOUND = 1000
+
+iteration = 0
+items = []
+items.append(0)
+while iteration < 10:
+    internal_check = 0
+    item_check = items[-1]
+    x = r.randint(LOWER_BOUND, UPPER_BOUND)
+    y = r.randint(LOWER_BOUND, UPPER_BOUND)
+    internal_check += 1
+    while x + y != item_check:
+        x = r.randint(LOWER_BOUND, UPPER_BOUND)
+        y = r.randint(LOWER_BOUND, UPPER_BOUND)
+        internal_check += 1
+    print(iteration, internal_check)
+    items.append(x)
+    items.append(y)
+    iteration += 1
+
+print(items)
