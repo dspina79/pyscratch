786a080e6541cb70595c78ee44a0d6599804192d
Dave Spina <dspina79@gmail.com>
2022-05-13 22:59:33 -0400

    New error type definition

--- errors/errortypes/newerrortype.py
+++ errors/errortypes/newerrortype.py
@@ -3,6 +3,10 @@ class MyError(Exception):
         super().__init__()
 
 
+class InvalidNameError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 try:
     x = 10
     if x < 20:
@@ -11,3 +15,13 @@ try:
         print('My error was not raised.')
 except MyError:
     print('My Error was raised')
+
+
+try:
+    inpropername = 'Divad'
+    if inpropername != 'David':
+        raise InvalidNameError("You cannot use a name linke that.")
+    else:
+        print("Welcome aboard")
+except InvalidNameError:
+    print(InvalidNameError.args)
