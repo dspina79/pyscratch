f691d96d161b4b134d305aa4ffc63f4c9cf68209
Dave Spina <dspina79@gmail.com>
2022-05-16 11:03:23 -0400

    More updates

--- random/graphing.py
+++ random/graphing.py
@@ -1,11 +1,15 @@
 import random as r
 
+MAX_INT = 10
 numbs = {}
-for i in range(0, 100):
-    x = r.randint(1, 10)
+
+for i in range(1, MAX_INT + 1):
+    numbs[i] = 0
+
+for i in range(0, 10000):
+    x = r.randint(1, MAX_INT)
     n = numbs.get(x, 0)
     n += 1
-
     numbs[x] = n
 
 print(numbs)
