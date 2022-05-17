ac3ebeb118273d8e937311ab24455aa650a5e5f0
Dave Spina <dspina79@gmail.com>
2022-05-17 10:44:07 -0400

    Updated raising error

--- errors/errortypes/newerrortype.py
+++ errors/errortypes/newerrortype.py
@@ -4,9 +4,14 @@ class MyError(Exception):
 
 
 class InvalidNameError(Exception):
+    def __init__(self, message):
+        self.message = message
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        if args.__len__() > 0:
+            self.message = args[0]
+        else:
+            self.message = "Unknown details"
 try:
     x = 10
     if x < 20:
@@ -20,8 +25,9 @@ except MyError:
 try:
     inpropername = 'Divad'
     if inpropername != 'David':
-        raise InvalidNameError("You cannot use a name linke that.")
+        raise InvalidNameError("You cannot use a name like that.")
     else:
         print("Welcome aboard")
-except InvalidNameError:
-    print(InvalidNameError.args)
+except InvalidNameError as err:
+    print("The error was thrown")
+    print(err.message)
