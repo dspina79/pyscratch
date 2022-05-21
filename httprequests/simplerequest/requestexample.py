1424332dbd31f4cdbc2c5d23a16458e14aebaec8
Dave Spina <dspina79@gmail.com>
2022-05-21 17:58:05 -0400

    Added posting

--- httprequests/simplerequest/requestexample.py
+++ httprequests/simplerequest/requestexample.py
@@ -1,9 +1,26 @@
 import pip._vendor.requests as requests
 
-def get_toto(id):
+class Todo:
+    def __init__(self):
+        super().__init__()
+        self.userId = 0
+        self.title = ""
+        self.completed = False
+
+def get_todo(id):
     url = "https://jsonplaceholder.typicode.com/todos/" + str(id)
     resp = requests.get(url)
     print(resp.json())
+    data = resp.json()
+    print(data['title'])
 
+def make_todo(userid, title, completed):
+    body = {"userId": userid, "title": title, "completed": completed}
+    url = "https://jsonplaceholder.typicode.com/todos"
+    resp = requests.post(url, data = body)
+    data = resp.json()
+    return data["id"]
 
-get_toto(2)
+get_todo(2)
+result = make_todo(1, "Another Test", False)
+print(result)
