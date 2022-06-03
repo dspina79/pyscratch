2249bd82a7bf4809269c32fb376fede13e0243ae
Dave Spina <dspina79@gmail.com>
2022-06-02 22:10:56 -0400

    Added better timing mechanism

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -1,11 +1,13 @@
 import time
 
-def countdown(sec):
-    while sec:
-        print(sec, end="\r")
+def countdown(totalsec):
+    while totalsec:
+        min, sec = divmod(totalsec, 60)
+        timer = "{:02d}:{:02d}".format(min, sec)
+        print(timer, end="\r")
         time.sleep(1)
-        sec -= 1
+        totalsec -= 1
+    print("BEEP BEEP")
 
-
-t = input("Enter the time in seconds")
+t = input("Enter the time in seconds: ")
 countdown(int(t))
