86776c963c6d1362fb39c0efcbb4a06f2a378d90
Dave Spina <dspina79@gmail.com>
2022-06-05 13:09:08 -0400

    Fininalized first draft of number guesser

--- games/betterguess/betterguess.py
+++ games/betterguess/betterguess.py
@@ -9,16 +9,25 @@ class BetterGuess:
         self.won = False
 
     def play_game(self):
-        current_val = int(input("Pick a number between 1 and " + self.max_number))
         current_guesses = 0
-
+        self.acutal_number = r.randint(1, self.max_number)
         while current_guesses < self.max_guesses:
+            scurrent_val = input("Pick a number between 1 and " + str(self.max_number) + ": ")
+            current_val = int(scurrent_val)
             current_guesses += 1
             if current_val == self.acutal_number:
                 self.won = True
                 break
+            elif current_val < self.acutal_number:
+                print("Think higher")
+            else:
+                print("Think lower")
+            print("You have", str(self.max_guesses - current_guesses), "left")
 
         if not self.won:
-            print("You lost. The actual number was", self.acutal_number)
+            print("You lost. The actual number was", str(self.acutal_number))
         else:
             print("You won!")
+
+g = BetterGuess(100)
+g.play_game()
