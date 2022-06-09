800b3cf50c28c713f7e77827d4cc6d1c7066b020
Dave Spina <dspina79@gmail.com>
2022-06-08 22:50:41 -0400

    Implemented standard deviation

--- mather/stats.py
+++ mather/stats.py
@@ -8,6 +8,19 @@ def generate_average(nums):
             total += x
         return total / n
 
+def std_dev(nums):
+    n = nums.__len__()
+    if n == 0:
+        return None
+    else:
+        avg = generate_average(nums)
+        numerator = 0
+        for n in nums:
+            numerator = (n - avg) ** 2
+        quot = numerator / nums.__len__()
+        return quot ** (1/2)
+
+
 nums = []
 while True:
     current_intput = input("Get a number or blank to end: ")
@@ -18,3 +31,5 @@ while True:
 
 avg = generate_average(nums)
 print("The average is", avg)
+stddev = std_dev(nums)
+print("The standard deviation is", stddev)
