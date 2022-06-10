6f572c29169c011dcb6a87d26ce3d2d10219c183
Dave Spina <dspina79@gmail.com>
2022-06-10 18:18:08 -0400

    Added console clearing

--- sleep/waiting.py
+++ sleep/waiting.py
@@ -1,7 +1,15 @@
 import time as t
+import os
+
+def cls():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        comamnd = 'cls'
+    os.system(command)
 
 def countdown(high):
     while high > 0:
+        cls() # clear the console
         print(high)
         t.sleep(1)
         high -= 1
