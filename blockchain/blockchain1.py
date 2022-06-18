39a3882ca0ba99a68efc95dbf55de20556b26373
Dave Spina <dspina79@gmail.com>
2022-06-18 14:53:53 -0400

    Added blockchain

--- blockchain/blockchain1.py
+++ blockchain/blockchain1.py
@@ -1,5 +1,6 @@
 from hashlib import sha256
 import json as j
+import time as t
 
 class Block:
     def __init__(self, index, transactions, timestamp, previous_hash, nonce = 0):
@@ -12,3 +13,18 @@ class Block:
     def compute_hash(self):
         block_string = j.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], t.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
