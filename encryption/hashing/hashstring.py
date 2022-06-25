2171757c4c33def8c4289a7f5f9b5cf68ca0f2b2
Dave Spina <dspina79@gmail.com>
2022-06-24 22:40:38 -0400

    Added storage of passwords

--- encryption/hashing/hashstring.py
+++ encryption/hashing/hashstring.py
@@ -3,6 +3,10 @@ import _sha256 as s
 def hash_string(str):
     return s.sha256(str.encode()).hexdigest()
 
+def write_password(password):
+    with open("encryption/hashing/passtext.txt", "w", encoding="utf-8") as f:
+        f.writelines(password)
+
 def run():
     start_password = input("Enter a password: ")
     hash_start = hash_string(start_password)
@@ -10,8 +14,9 @@ def run():
     confirm_password = input("Confirm the password: ")
     hash_confirm = hash_string(confirm_password)
 
-    if start_password == confirm_password:
+    if hash_start == hash_confirm:
         print("Passwords match")
+        write_password(hash_confirm)
     else:
         print("The passwords do not match.")
 
