062e209f8e13bd9337c3bf1603565d3801403d62
Dave Spina <dspina79@gmail.com>
2022-06-26 21:47:33 -0400

    Added more to using Fernet

--- encryption/basickey/basickeyencryption.py
+++ encryption/basickey/basickeyencryption.py
@@ -1,7 +1,14 @@
+from cryptography.fernet import Fernet
 
-def encrypt(str):
-    pass
+class MyEncryptor:
+    def __init__(self):
+        super().__init__()
+        self.key = Fernet.generate_key()
+        self.crypto = Fernet(self.key)
 
-def decrypt(eStr):
-    pass
+    def encrypt(plaintext):
+        return crypto.encrypt(bytes(plaintext, encoding="utf-8"))
+
+    def decrypt(encrypted_string):
+        pass
 
