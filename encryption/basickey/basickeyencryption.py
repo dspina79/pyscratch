55cdfc8ba7344544544f06dcfa9ce5c5c27b4c9c
Dave Spina <dspina79@gmail.com>
2022-06-27 21:41:50 -0400

    Added decryption

--- encryption/basickey/basickeyencryption.py
+++ encryption/basickey/basickeyencryption.py
@@ -9,6 +9,11 @@ class MyEncryptor:
     def encrypt(plaintext):
         return crypto.encrypt(bytes(plaintext, encoding="utf-8"))
 
-    def decrypt(encrypted_string):
-        pass
+    def decrypt(encrypted_bytes):
+        db = crypto.decrypt(encrypted_bytes)
+        return str(db, encoding="utf-8")
 
+m = MyEncryptor()
+b = m.encrypt("this is my encrypted string")
+ds = m.decrypt(b)
+print(ds)
