00e8ad1ba1d8945690938fe1587af39a873f1c4f
Dave Spina <dspina79@gmail.com>
2022-07-05 21:17:07 -0400

    Added more methodology and sleeping to pooling

--- parallelism/pooling/poolexample.py
+++ parallelism/pooling/poolexample.py
@@ -4,9 +4,18 @@ import time
 def cube (x):
     return x * x * x
 
+def square(x):
+    print("Squaring", x)
+    time.sleep(3)
+    return x * x
+
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=3)
     inputs = [1,2,4,6,8,9,123]
     outputs = pool.map(cube, inputs)
+    outputs_sq = pool.map(square, inputs)
     print("Inputs", inputs)
-    print("Outputs", outputs)
+    print("Outputs Cube", outputs)
+    print("Outputs Squared", outputs_sq)
+
+    #items are running is batches of 3 at a time
